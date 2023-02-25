2e277e98ae28c1e17536631e26e99aa91dcd9afb
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2023-02-24 18:10:57 -0700

    prints the leads saved in the array

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let myLeads = []
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("button clicked from addEventListener")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+
+for (let i = 0; i <myLeads.length; i++){
+    ulEl.textContent += myLeads[i] + " "
+}
