96c10c1b69f42e3a6b84a601d4df041842c0665b
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2023-03-02 19:57:22 -0700

    delete and save buttons added and styled along with funcitonality for both. also refactored the renderLeads function

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 let myLeads = []
+let oldLeads = []
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
+const tabs = [
+    {url: "https://www.linkedin.com/in/trevor-farquhar-908b42256/"}
+]
 
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-    renderLeads()
-    
+tabBtn.addEventListener("click", function(){
+    myLeads.push(tabs[0].url)
 })
 
-
-function renderLeads(){
+function renderLeads(applicationLinks){
     let listItems = ""
-    for (let i = 0; i <myLeads.length; i++){
+    for (let i = 0; i <applicationLinks.length; i++){
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}
+                <a target='_blank' href='${applicationLinks[i]}'>${applicationLinks[i]}
                 </a>
             </li>
         `
@@ -35,3 +35,22 @@ function renderLeads(){
 
     ulEl.innerHTML = listItems
 }
+
+
+deleteBtn.addEventListener("click", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    renderLeads(myLeads)
+    
+})
+
+
